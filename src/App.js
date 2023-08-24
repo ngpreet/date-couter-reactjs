ccbdf2c6b3eb76f1d7976fc8afc76a6e234607df
ngpreet <ng.navpreet@gmail.com>
2023-08-24 10:19:45 +0530

    date counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h3>Date Counter</h3>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleIncreaseStep() {
+    setStep((s) => s + 1);
+  }
+
+  function handleDecreaseStep() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleDecreaseStep}>-</button>
+        Step: {step}
+        <button onClick={handleIncreaseStep}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        Count: {count}
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        {count === 0
+          ? "Today is "
+          : count > 0
+          ? `${count} days from today is `
+          : `${Math.abs(count)} days ago was `}
+        {date.toDateString()}
+      </p>
     </div>
   );
 }
